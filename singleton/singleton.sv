30ac905bcea033b55c8ad867f7df6d861becdf76
chenxudong <chenxudong0318@gmail.com>
2023-05-04 20:58:12 +0800

    try singleton in multi-threads

--- singleton/singleton.sv
+++ singleton/singleton.sv
@@ -1,53 +1,62 @@
 /*----------------------------------------------------------*\
-    FILE: singleton.sv
-    AUTHOR:
+        FILE: singleton.sv
+        AUTHOR:
 
-    ABSTRACT: 
-    KEYWORDS: 
+        ABSTRACT: 
+        KEYWORDS: 
 
-    MODIFICATION HISTORY: 
-    $Log$
+        MODIFICATION HISTORY: 
+        $Log$
 
 \*----------------------------------------------------------*/
 
 // local means: sub-classes cannot access this variable
 // protected means: external functions cannot access
 class fruit;
-  static local fruit   m_self = null  ; // this local member should be static
-  static local string  m_name = ""    ;
+    static local fruit m_self = null; // this local member should be static
+    static local string m_name = "";
 
-  // new function should be proteced
-  protected function new();
-  endfunction
+    // new function should be proteced
+    protected function new();
+    endfunction
 
-  virtual function string set_name(string name);
-    m_name = name;
-  endfunction
+    virtual function string set_name(string name);
+        m_name = name;
+    endfunction
 
-  virtual function string get_name();
-    return m_name;
-  endfunction
+    virtual function string get_name();
+        return m_name;
+    endfunction
 
-  // this function should be static
-  static function fruit get();
-    if(m_self==null)
-      m_self = new();
-    return m_self;
-  endfunction
+    // this function should be static
+    static function fruit get();
+        if (m_self==null) begin
+            m_self = new();
+            $display("%0t --> Create singleton instance", $time);
+        end
+        return m_self;
+    endfunction
 endclass
 
 module tb_top;
-  initial
-  begin
-    fruit a = fruit::get();
-    a.set_name("hello");
-    $display(a.get_name());
-  end
-  //initial
-  //begin
-  //  fruit b = new();
-  //  b.set_name("hello");
-  //  $display(b.get_name());
-  //end
+    initial begin
+        fork
+            begin: thread_1
+                fruit a = fruit::get();
+                a.set_name("hello");
+                $display(a.get_name());
+            end
+            begin: thread_2
+                fruit b = fruit::get();
+                b.set_name("world");
+                $display(b.get_name());
+            end
+        join
+    end
+    //initial begin
+    //    fruit b = new();
+    //    b.set_name("hello");
+    //    $display(b.get_name());
+    //end
 endmodule
 
